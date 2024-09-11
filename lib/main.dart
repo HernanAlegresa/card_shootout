9d617f2f5c7d4c23119b861b5ae2af9102f00cae
HernanAlegresa <alegresah@gmail.com>
2024-09-11 19:02:27 -0300

    Implementación básica de la tanda de penales: - Se implementó una lógica completa para una tanda de penales, con reglas para el mejor de 5 y modalidad mata a mata en caso de empate. - Se permite al usuario ejecutar penales y también atajar con un cuadrado 3x3 que se ajusta a la matriz del arco. - Se agregó una visualización en pantalla para indicar cuándo el jugador o la CPU ganan. - Se corrigió el problema del hover en los bordes de la matriz al ejecutar penales. - Se añadió la funcionalidad de reinicio del juego después de finalizar la tanda. - Actualización del marcador para cada gol o atajada. - Organización del código para mejorar la claridad y funcionalidad.

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,26 @@ class PenaltyShootoutApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: PenaltyGame(), // Llama a la clase
+      home: PenaltyGame(),
     );
   }
 }
 
 class PenaltyGame extends StatefulWidget {
-
   @override
   _PenaltyGameState createState() => _PenaltyGameState();
 }
 
 class _PenaltyGameState extends State<PenaltyGame> {
-  //----VARIABLES----//
   int playerScore = 0;
   int cpuScore = 0;
+  int playerPenalties = 0;
+  int cpuPenalties = 0;
   bool isPlayerTurn = true;
-  List<List<int>> playerSelectedTiles = []; // En esta var se guardan los cuadrados selecionados del usr
-  List<List<int>> cpuSelectedTiles = []; // En esta var se guardan los cuadrados selecionados del CPU
-  List<int>? hoveredTile;  // Nueva variable para el hover
-  //----VARIABLES----//
+  bool gameEnded = false;
+  List<List<int>> playerSelectedTiles = [];
+  List<List<int>> cpuSelectedTiles = [];
+  List<int>? hoveredTile;
 
   @override
   Widget build(BuildContext context) {
@@ -41,164 +41,205 @@ class _PenaltyGameState extends State<PenaltyGame> {
       appBar: AppBar(
         title: Text("Penalty Shootout"),
       ),
-      body: Center( // el child column donde se guarda la grid hereda del body Center para que todos los elementos esten centrados
-        child: Column( //hijo donde se va a almacenar la grid
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [ //TODA LA GRID
-          Text(
-            "Elián Fc: $playerScore | CPU: $cpuScore",
-            style: TextStyle(fontSize: 24),
-          ),
-          SizedBox(height: 20),
-          Expanded(
-            child: SizedBox(
-              width: 350, 
-              height: 250,
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 7, //columnas
-                  mainAxisSpacing: 4.0, //espaciado entre filas
-                  crossAxisSpacing: 4.0, //espaciado entre columnas
-                  childAspectRatio: 1.0, //Para que la grid sean cuadraditos
-                ),
-                itemCount: 35, //35 cuadraditos selecionables (5x7 = 35)
-                itemBuilder: (context, index) {
-                  int row = index ~/ 7; // Calculate row from index
-                  int col = index % 7;  // Calculate column from index
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Hernan: $playerScore | CPU: $cpuScore",
+              style: TextStyle(fontSize: 24),
+            ),
+            if (gameEnded)
+              Column(
+                children: [
+                  Text(
+                    playerScore > cpuScore ? 'Jugador Gana!' : 'CPU Gana!',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                  ElevatedButton(
+                    onPressed: resetGame,
+                    child: Text('Reiniciar Tanda de Penales'),
+                  ),
+                ],
+              ),
+            SizedBox(height: 20),
+            Expanded(
+              child: SizedBox(
+                width: 350,
+                height: 250,
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 7,
+                    mainAxisSpacing: 4.0,
+                    crossAxisSpacing: 4.0,
+                    childAspectRatio: 1.0,
+                  ),
+                  itemCount: 35,
+                  itemBuilder: (context, index) {
+                    int row = index ~/ 7;
+                    int col = index % 7;
 
-                  return MouseRegion(
-                    onEnter: (_) => _onHoverEnter(row, col),  // Detectar cuando el mouse entra en el cuadrado
-                    onExit: (_) => _onHoverExit(),  // Detectar cuando el mouse sale del cuadrado
-                    child: GestureDetector( // checkear el estado del bool playerturn para saber si el usr ataja o patea
-                      onTap: () {
-                        if (isPlayerTurn) {
-                          handlePlayerShoot(row, col); //si es el turno del player, llama a la funcion para patear
-                        } else {
-                          handlePlayerSave(row, col); //si es false, por ende el player ataja
-                        }
-                      },
-                      child: GridTile( // Estilo de cada cuadrado
-                        child: Container(
-                          margin: const EdgeInsets.all(4.0),
-                          decoration: BoxDecoration(
-                            color: getColorForTile(row, col),
-                            border: Border.all(color: Colors.black),
+                    return MouseRegion(
+                      onEnter: (_) => _onHoverEnter(row, col),
+                      onExit: (_) => _onHoverExit(),
+                      child: GestureDetector(
+                        onTap: () {
+                          if (!gameEnded) {
+                            if (isPlayerTurn) {
+                              handlePlayerShoot(row, col);
+                            } else {
+                              handlePlayerSave(row, col);
+                            }
+                          }
+                        },
+                        child: GridTile(
+                          child: Container(
+                            margin: const EdgeInsets.all(4.0),
+                            decoration: BoxDecoration(
+                              color: getColorForTile(row, col),
+                              border: Border.all(color: Colors.black),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 20)
-        ],
-      ),
+            const SizedBox(height: 20)
+          ],
+        ),
       ),
     );
   }
 
-  // Método para manejar la entrada del cursor en una casilla
   void _onHoverEnter(int row, int col) {
-    setState(() {
-      hoveredTile = [row, col];
-    });
+    if (isPlayerTurn || (row > 0 && row < 4 && col > 0 && col < 6)) {
+      setState(() {
+        hoveredTile = [row, col];
+      });
+    }
   }
 
-  // Método para manejar la salida del cursor de una casilla
   void _onHoverExit() {
     setState(() {
       hoveredTile = null;
     });
   }
 
-  //----LOGICA DE PATEAR PLAYER----//
   void handlePlayerShoot(int row, int col) {
     setState(() {
       playerSelectedTiles = [
         [row, col]
       ];
-      // una vez guarda los valores en pselectedtiles, llama a la f para que la cpu ataje random y la que compara para ver si es gol
       cpuSelectSaveZone();
       checkGoalOrSave();
-      // Esperamos un momento para que el usuario vea la jugada, luego limpiamos la matriz
       Future.delayed(Duration(seconds: 1), () {
-        clearMatrix(); // Limpiar la matriz
+        clearMatrix();
         setState(() {
-          isPlayerTurn = false; // Cambiamos el turno al portero
+          isPlayerTurn = false;
+          playerPenalties++;
+          if (!gameEnded) handleNextRound();
         });
       });
     });
   }
 
-  //----LOGICA DE ATAJAR PLAYER----//
   void handlePlayerSave(int row, int col) {
-    setState(() {
-      playerSelectedTiles = get3x3Zone(row, col);
-      cpuSelectShootZone();
-      checkGoalOrSave();
-
-      // Esperamos un momento para que el usuario vea la jugada, luego limpiamos la matriz
-      Future.delayed(Duration(seconds: 1), () {
-        clearMatrix(); // Limpiar la matriz
-        setState(() {
-          isPlayerTurn = true; // Cambiamos el turno al ejecutor
+    if (row > 0 && row < 4 && col > 0 && col < 6) {
+      setState(() {
+        playerSelectedTiles = get3x3Zone(row, col);
+        cpuSelectShootZone();
+        checkGoalOrSave();
+        Future.delayed(Duration(seconds: 1), () {
+          clearMatrix();
+          setState(() {
+            isPlayerTurn = true;
+            cpuPenalties++;
+            if (!gameEnded) handleNextRound();
+          });
         });
       });
+    }
+  }
+
+  void handleNextRound() {
+    // Si ambos jugadores han realizado la misma cantidad de disparos, verificar si el juego debe continuar
+    if (playerPenalties == cpuPenalties) {
+      if (playerPenalties < 5) {
+        // Continuar la tanda regular
+        return;
+      } else if (playerPenalties == 5) {
+        // Evaluar si hay un ganador tras los primeros 5 penales
+        if (playerScore != cpuScore) {
+          endGame();
+        }
+        // Si hay empate, continuar en muerte súbita
+      } else {
+        // En la muerte súbita, solo declarar ganador si la diferencia es de un gol tras ambas ejecuciones
+        if ((playerScore > cpuScore) || (cpuScore > playerScore)) {
+          endGame();
+        }
+      }
+    }
+  }
+
+  void endGame() {
+    setState(() {
+      gameEnded = true;
     });
   }
 
-  // Función para limpiar la matriz entre turnos
   void clearMatrix() {
     playerSelectedTiles.clear();
     cpuSelectedTiles.clear();
   }
 
+  void resetGame() {
+    setState(() {
+      playerScore = 0;
+      cpuScore = 0;
+      playerPenalties = 0;
+      cpuPenalties = 0;
+      isPlayerTurn = true;
+      gameEnded = false;
+      clearMatrix();
+    });
+  }
+
   List<List<int>> get3x3Zone(int row, int col) {
     List<List<int>> selectedTiles = [];
-
-    // Ajustar los límites para asegurar que el cuadrado 3x3 no se salga de la matriz
     int startRow = (row - 1 < 0) ? 0 : (row + 1 > 4) ? 2 : row - 1;
     int startCol = (col - 1 < 0) ? 0 : (col + 1 > 6) ? 4 : col - 1;
-
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
         selectedTiles.add([startRow + i, startCol + j]);
       }
     }
-
-    return selectedTiles; // Retornar las casillas ajustadas
+    return selectedTiles;
   }
 
-  //----LOGICA CPU ATAJAR----//
   void cpuSelectSaveZone() {
     setState(() {
-      cpuSelectedTiles = getRandom3x3Zone(); // Cambiamos a un área 3x3
+      int row = Random().nextInt(3) + 1;
+      int col = Random().nextInt(5) + 1;
+      cpuSelectedTiles = get3x3Zone(row, col);
     });
   }
 
-  //----CPU 3X3 RANDOM----//
-  List<List<int>> getRandom3x3Zone() {
-    int row = Random().nextInt(5); // Limitamos a 5 para que no se salga del borde inferior
-    int col = Random().nextInt(7); // Limitamos a 7 para que no se salga del borde derecho
-    return get3x3Zone(row, col); // Reutilizamos la misma lógica para obtener el área 3x3
-  }
-
-  //----LOGICA DE PATEO CPU----//
   void cpuSelectShootZone() {
     setState(() {
-      int row = Random().nextInt(5); // Random() para elegir coords al azar en una de las 5 filas
-      int col = Random().nextInt(7); // Random() para elegir coords al azar en una de las 7 columnas
+      int row = Random().nextInt(5);
+      int col = Random().nextInt(7);
       cpuSelectedTiles = [
         [row, col]
       ];
     });
   }
 
-  //----COMPROBACIONES PARA VER SI ES GOL----//
   void checkGoalOrSave() {
-    if (isPlayerTurn) { //si es el turno del usr, se comprueba si mete gol, si sí, incrementa la var playerscore
+    if (isPlayerTurn) {
       if (cpuSelectedTiles.any((tile) => tile[0] == playerSelectedTiles[0][0] && tile[1] == playerSelectedTiles[0][1])) {
         print("CPU atajo");
       } else {
@@ -206,7 +247,6 @@ class _PenaltyGameState extends State<PenaltyGame> {
         print("USR gol");
       }
     } else {
-      //si es el turno de patear de la cpu, se comprueba si mete gol, si sí, incrementa la var playerscore
       if (playerSelectedTiles.any((tile) => tile[0] == cpuSelectedTiles[0][0] && tile[1] == cpuSelectedTiles[0][1])) {
         print("USR atajo");
       } else {
@@ -216,61 +256,38 @@ class _PenaltyGameState extends State<PenaltyGame> {
     }
   }
 
-// Método para obtener el área 3x3 alrededor del cuadrado donde está el cursor
-List<List<int>> getHovered3x3Zone(int row, int col) {
-  List<List<int>> hoveredTiles = [];
-
-  for (int i = -1; i <= 1; i++) {
-    for (int j = -1; j <= 1; j++) {
-      int newRow = row + i;
-      int newCol = col + j;
-      // Nos aseguramos de que no salimos de los límites de la matriz (7x5)
-      if (newRow >= 0 && newRow < 5 && newCol >= 0 && newCol < 7) {
-        hoveredTiles.add([newRow, newCol]);
+  Color getColorForTile(int row, int col) {
+    if (cpuSelectedTiles.any((tile) => tile[0] == row && tile[1] == col)) {
+      return Colors.red;
+    } else if (playerSelectedTiles.any((tile) => tile[0] == row && tile[1] == col)) {
+      return Colors.blue;
+    } else if (hoveredTile != null) {
+      if (!isPlayerTurn && isInHovered3x3Zone(row, col)) {
+        return const Color.fromARGB(255, 91, 195, 107);
+      } else if (isPlayerTurn && hoveredTile![0] == row && hoveredTile![1] == col) {
+        return const Color.fromARGB(255, 91, 195, 107);
       }
     }
+    return Colors.white;
   }
 
-  return hoveredTiles; // Retornamos las casillas alrededor del hover
-}
-
-  // Actualización del método para determinar el color de cada casilla
-Color getColorForTile(int row, int col) {
-  // Priorizar el disparo de la CPU si coincide con una casilla
-  if (cpuSelectedTiles.any((tile) => tile[0] == row && tile[1] == col)) {
-    return Colors.red; // Mostrar el disparo de la CPU (rojo)
-  } 
-  // Mostrar el área seleccionada por el usuario
-  else if (playerSelectedTiles.any((tile) => tile[0] == row && tile[1] == col)) {
-    return Colors.blue; // Mostrar el área seleccionada por el usuario (celeste)
-  } 
-  // Mostrar el hover del usuario para el área 3x3
-  else if (hoveredTile != null) {
-    if (!isPlayerTurn) {
-      // Si el usuario está atajando, mostrar el área 3x3
-      if (isInHovered3x3Zone(row, col)) {
-        return const Color.fromARGB(255, 91, 195, 107); // Color para el área 3x3
-      }
-    } else {
-      // Si el usuario está pateando, mostrar solo la casilla debajo del cursor
-      if (hoveredTile![0] == row && hoveredTile![1] == col) {
-        return const Color.fromARGB(255, 91, 195, 107); // Color para el hover en una sola casilla
+  List<List<int>> getHovered3x3Zone(int row, int col) {
+    List<List<int>> hoveredTiles = [];
+    for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
+        int newRow = row + i;
+        int newCol = col + j;
+        if (newRow >= 0 && newRow < 5 && newCol >= 0 && newCol < 7) {
+          hoveredTiles.add([newRow, newCol]);
+        }
       }
     }
+    return hoveredTiles;
   }
-  
-  return Colors.white; // Casillas no seleccionadas
-}
 
-
-
-  // Función para verificar si una casilla está en el área 3x3 alrededor del cursor
   bool isInHovered3x3Zone(int row, int col) {
     if (hoveredTile == null) return false;
-
     List<List<int>> hoveredArea = getHovered3x3Zone(hoveredTile![0], hoveredTile![1]);
-
     return hoveredArea.any((tile) => tile[0] == row && tile[1] == col);
   }
-
 }
